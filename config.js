5387cc6163a32cb8379621c0842001eb7b153e65
linasviridova77-sketch <linasviridova77@gmail.com>
2026-08-15 20:25:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // ANON KEY является публичным ключом клиентского приложения.
 // Безопасность данных обеспечивается RLS-политиками в Supabase.
 window.FIN_CONFIG = {
-  SUPABASE_URL: "https://heccswxhkwowaqreawon.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://heccswxhkwowaqreawon.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_HN0Q46Tvzv4DCSsO-eH5lw_yvAGQ0vP"
 };
